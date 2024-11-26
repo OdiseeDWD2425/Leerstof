f4ad88e1ad2f2d34bf16692f60b1a5bb9bdfdd9c
jensbaetens <jens.baetens@ikdoeinformatica.be>
2024-11-26 20:11:46 +0100

    week 1 deel 2

--- Week 1/demo.js
+++ Week 1/demo.js
@@ -35,9 +35,9 @@ const nullObject = null; // redelijk analoog aan undefined;
 
 // let op met getallen als tekst voor te stellen -> javascript kan het concateneren ipv optellen
 let tmp = 31;
-console.log(tmp+1);
+console.log(tmp + 1);
 tmp = '31';
-console.log(tmp+1);
+console.log(tmp + 1);
 
 // wiskundige operaties gelijkaardig aan csharp
 tmp + 31;
@@ -68,3 +68,98 @@ person -> plaats 0x10321901 in het geheugen
                     -> lastname
 const wil zeggen dat 0x10321901 constant moet blijven
 */
+
+// Arrays (met rechte haakjes)
+const animals = ['Dog', 'Cat', 'Fish'];
+
+animals[2] = 'Goudvis';
+animals[50] = 'Alpaca'; // kan niet in csharp
+animals[10] = 1010.10; // er kunnen meerdere types in 1 array zitten
+
+console.log(animals[50]); // undefined als het buiten de range valt
+
+animals.push('Geit'); // voegt het element achteraan toe
+console.log(animals.pop()); // omgekeerde van push
+animals.unshift('Kikker'); // zelfde als push maar vanvoor
+console.log(animals.shift()); // omgekeerde van unshift
+
+console.log(animals);
+
+// Controlestructuren
+// if-else
+const age = 20;
+
+if (age <= 18) {
+	console.log('doe dit');
+} else if (age >= 65) {
+	console.log('doe dat');
+} else {
+	console.log('doe niets');
+}
+
+// switch
+switch (age) {
+case 20:
+	console.log('doe dit');
+	break; // waarom is dit belangrijk? anders gaat javascript verder met de code in de volgende case!
+case 65:
+	console.log('doe dat');
+	break;
+default:
+	console.log('doe niets');
+}
+
+// for-lus
+for (let i = 0; i < 10; i++) {
+	console.log(i);
+}
+for (let animal in animals) {
+	console.log(animal); // dit print de indices/de properties van een object/ de keys van een dictionary uit
+	console.log(animals[animal]);
+}
+
+for (let animal of animals) { // met of in de for print je de values uit, met in de keys/indices
+	console.log(animal);
+}
+animals.forEach((value, index) => { // dit is een lambda expressie (een functie die opgeroepen wordt voor elke functie in de array)
+	console.log(value);
+	console.log(index);
+});
+
+// while
+let counter = 20;
+while(counter < 10){
+	counter ++;
+	console.log(counter);
+}
+
+// do-while = analoog aan de while maar de lus wordt minstens 1 keer uitgevoerd
+do {
+	counter ++;
+	console.log(counter);
+} while(counter < 10);
+
+//functions
+function printHelloWorld(){
+	console.log('hello world!');
+}
+printHelloWorld();
+
+function helloworld(name){
+	return 'hello ' + name;
+}
+console.log(helloworld('Jens'));
+
+// self invoking function -> de meest strikte manier om een main te maken
+(function(){
+	// hier willen we in principe alles in coderen
+	// zodat de file op zichzelf staat
+	// we maken een naamloze functie hiervoor
+
+	// dit is belangrijk voor de scope van variabelen
+	let x = 10;
+
+})(); // met de laatste twee haakjes wordt deze naamloze functies onmiddelijk uitgevoerd
+
+let x = 20;
+
